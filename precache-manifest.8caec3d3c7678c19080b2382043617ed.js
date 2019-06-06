67fad976732c0c56d1db35f8f4acea515db59411
sholomon.pinoliad <sholomon.pinoliad@studentpartner.com>
2019-06-06 21:26:34 +0800

    Version 1.0.0

---
+++ precache-manifest.8caec3d3c7678c19080b2382043617ed.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9270ad912b990a0271936af172cba8f7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5c8ec17787bb07a42531",
+    "url": "/static/css/main.8c1463f4.chunk.css"
+  },
+  {
+    "revision": "f206d0b3f7409470aecb",
+    "url": "/static/js/2.8455007e.chunk.js"
+  },
+  {
+    "revision": "5c8ec17787bb07a42531",
+    "url": "/static/js/main.df810f9a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "89e02d41446b73046edf09d934441f8a",
+    "url": "/static/media/close-envelope.89e02d41.svg"
+  },
+  {
+    "revision": "6c61df9b2018a79a547662b4a068455a",
+    "url": "/static/media/confident.6c61df9b.svg"
+  },
+  {
+    "revision": "abda0db200c38a85e612fdeb18082c49",
+    "url": "/static/media/cover-photo.abda0db2.png"
+  },
+  {
+    "revision": "e729c695a48748a49d542ff5234022ec",
+    "url": "/static/media/dynamic.e729c695.svg"
+  },
+  {
+    "revision": "c6f60b42ed044ff8c5635c5fafddb1dc",
+    "url": "/static/media/instagram.c6f60b42.svg"
+  },
+  {
+    "revision": "a1baa9e9034ba7cd3534f29718c5e7b9",
+    "url": "/static/media/linkedin-logo.a1baa9e9.svg"
+  },
+  {
+    "revision": "66df8206a15b0a0ffb275fde7b496bb0",
+    "url": "/static/media/motivated.66df8206.svg"
+  }
+]);
